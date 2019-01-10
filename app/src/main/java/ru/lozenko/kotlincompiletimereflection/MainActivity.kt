342289b352c72f71c55af180d2dca40af554d0b9
Ivan Lozenko <876l678@gmail.com>
2019-01-10 13:24:33 +0300

    small fix

--- app/src/main/java/ru/lozenko/kotlincompiletimereflection/MainActivity.kt
+++ app/src/main/java/ru/lozenko/kotlincompiletimereflection/MainActivity.kt
@@ -7,10 +7,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val settings by lazy { compileSettings }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        readSettingButton.setOnClickListener { setting_editText.text = SpannableStringBuilder(compileSettings.setting5) }
-        writeSettingButton.setOnClickListener { compileSettings.setting5 = setting_editText.text.toString() }
+        readSettingButton.setOnClickListener { setting_editText.text = SpannableStringBuilder(settings.setting5) }
+        writeSettingButton.setOnClickListener { settings.setting5 = setting_editText.text.toString() }
     }
 }
